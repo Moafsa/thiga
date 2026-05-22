dd0688168f7089b4ba48c0a827b451acc414b580
Moafsa <moaci@thiga.com.br>
2026-05-22 15:08:21 -0300

    fix: Add SuperAdminSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SuperAdminSeeder::class,
             PlanSeeder::class,
             RoleSeeder::class,
             TenantSeeder::class,
